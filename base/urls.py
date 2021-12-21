0d3d3fc237f810e19573f84e16360f5ef638193c
AayushGuragain <59627010+AayushGuragain@users.noreply.github.com>
2021-12-21 15:47:41 -0500

    second draft login/logout

--- base/urls.py
+++ base/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from .views import ItemList, ItemDetail, ItemCreate, ItemUpdate, ItemDelete
+from .views import ItemList, ItemDetail, ItemCreate, ItemUpdate, ItemDelete, CustomLoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path('login/',CustomLoginView.as_view(), name = 'login'),
+    path('logout/',LogoutView.as_view(next_page = 'login'), name = 'logout'),
     path('', ItemList.as_view(), name = 'items'),
     path('item/<int:pk>', ItemDetail.as_view(), name='item'),
     path('create-item/', ItemCreate.as_view(), name = 'item-create'),
     path('item-update/<int:pk>', ItemUpdate.as_view(), name='item-update'),
     path('item-delete/<int:pk>', ItemDelete.as_view(), name='item-delete'),
-
 
 ]
