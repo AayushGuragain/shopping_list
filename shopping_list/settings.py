97229701a3cd382107c183a695b11ac40df55825
AayushGuragain <59627010+AayushGuragain@users.noreply.github.com>
2021-12-22 16:40:51 -0500

    deployment to heroku, draft

--- shopping_list/settings.py
+++ shopping_list/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z+^cf1gjjg=)lw5p3v4n+@%7yl+$&i4n@!j9_^bibd2_s0ms52'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1',]
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
